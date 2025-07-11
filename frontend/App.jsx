11b2b1878e77dc2e103b1f0c32caa6d31213ea47
jcantonSilbon <jcanton@silbon.es>
2025-07-11 09:12:03 +0200

    .env

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,29 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { NavMenu } from "@shopify/app-bridge-react";
+import { AppProvider } from "@shopify/polaris";
 import Routes from "./Routes";
 
-import { QueryProvider, PolarisProvider } from "./components";
-
-export default function App() {
-  // Any .tsx or .jsx files in /pages will become a route
-  // See documentation for <Routes /> for more info
-  const pages = import.meta.glob("./pages/**/!(*.test.[jt]sx)*.([jt]sx)", {
-    eager: true,
-  });
-  const { t } = useTranslation();
-
+function App({ host }) {
   return (
-    <PolarisProvider>
-      <BrowserRouter>
-        <QueryProvider>
-          <NavMenu>
-            <a href="/" rel="home" />
-            <a href="/pagename">{t("NavigationMenu.pageName")}</a>
-          </NavMenu>
-          <Routes pages={pages} />
-        </QueryProvider>
-      </BrowserRouter>
-    </PolarisProvider>
+    <AppProvider
+      config={{
+        apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
+        host,
+        forceRedirect: true,
+      }}
+    >
+      <Routes />
+    </AppProvider>
   );
 }
+
+export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,8 +2,10 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 import { initI18n } from "./utils/i18nUtils";
 
-// Ensure that locales are loaded before rendering the app
 initI18n().then(() => {
+  const params = new URLSearchParams(window.location.search);
+  const host = params.get("host");
+
   const root = createRoot(document.getElementById("app"));
-  root.render(<App />);
+  root.render(<App host={host} />);
 });
